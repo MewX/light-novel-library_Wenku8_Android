4eb08b88c30010b5362218c8e7ff6c9539f16f3b
MewX <xiayuanzhong+github@gmail.com>
2023-07-15 20:48:29 -0700

    Added placeholder for the migration utility class

---
+++ studio-android/LightNovelLibrary/app/src/main/java/org/mewx/wenku8/util/SaveFileMigration.java
@@ -0,0 +1,24 @@
+package org.mewx.wenku8.util;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * The utility class for migration save files from prior-API-30 to API 30+ (Android R) world.
+ */
+public class SaveFileMigration {
+    public static boolean migrationCompleted() {
+        // TODO: create a file in internal storage that checks if the migration has completed or not.
+        return false;
+    }
+
+    public static List<String> generateMigrationPlan() {
+        // TODO: generate the list of files used for migration.
+        return Collections.emptyList();
+    }
+
+    public static boolean migrateFile(String filePath) {
+        // TODO
+        return true;
+    }
+}
