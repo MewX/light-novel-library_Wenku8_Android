36e6ee334ceb6db1d510f76c2578a9edd6f4e177
MewX <i@mewx.org>
2026-01-08 12:01:21 +1100

    Fixing a syntax error from AI

--- studio-android/LightNovelLibrary/app/src/main/java/org/mewx/wenku8/activity/NovelChapterActivity.java
+++ studio-android/LightNovelLibrary/app/src/main/java/org/mewx/wenku8/activity/NovelChapterActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
